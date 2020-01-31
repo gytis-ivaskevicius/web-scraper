f35928491dfc89fa831fab831205e11ebd2c4474
Gytis <gytis02.21@gmail.com>
2020-01-31 21:46:12 +0200

    Gradle config finishing touches

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,39 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile;
+
+plugins {
+    kotlin("jvm") version "1.3.61"
+}
 
 allprojects {
-    group = "org.example"
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    group = "com.gytis"
     version = "1.0-SNAPSHOT"
 
     repositories {
         mavenLocal()
         mavenCentral()
+    }
+
+    dependencies {
+        implementation(kotlin("stdlib-jdk8"))
+        testImplementation("io.kotlintest:kotlintest-runner-junit5:3.3.3")
+    }
+
+    tasks.withType<Test>().configureEach {
+        useJUnitPlatform()
+    }
+
+    tasks.withType<KotlinCompile>().configureEach {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xinline-classes")
+            jvmTarget = "1.8"
+        }
+    }
+}
+
+dependencies {
+    subprojects.forEach {
+        archives(it)
     }
 }
 
--- selenium/build.gradle.kts
+++ selenium/build.gradle.kts
@@ -1,34 +1,8 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-plugins {
-    kotlin("jvm") version "1.3.61"
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
     implementation("org.seleniumhq.selenium:selenium-chrome-driver:3.14.0")
     implementation("org.seleniumhq.selenium:selenium-api:3.14.0")
 
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jsoup:jsoup:1.12.1")
-
-    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.3.3")
-}
-
-tasks {
-    compileKotlin {
-        kotlinOptions.jvmTarget = "11"
-    }
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = "11"
-    }
-}
-
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    freeCompilerArgs = listOf("-Xinline-classes")
 }
